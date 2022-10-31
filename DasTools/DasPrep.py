55f076b3f08e1bb2691047ee13e3ddc6b054391c
Bin Luo <bluo@192.168.0.108>
2022-10-31 23:01:05 +0800

    update DasPrep.py

--- DasTools/DasPrep.py
+++ DasTools/DasPrep.py
@@ -21,23 +21,34 @@ def read_das(fname, **kwargs):
 def _read_das_h5(fname, **kwargs):
     
     h5_file = h5py.File(fname,'r')
-    nch = h5_file['Acquisition/Raw[0]/RawData/'].shape[1]
+    nch = h5_file['Acquisition'].attrs['NumberOfLoci']
     metadata = kwargs.pop('metadata', False)
     
     if metadata:
+        
         time_arr = h5_file['Acquisition/Raw[0]/RawDataTime/']
         dt = np.diff(time_arr).mean()/1e6
         nt = len(time_arr)
+        dx = h5_file['Acquisition'].attrs['SpatialSamplingInterval']
+        GL = h5_file['Acquisition'].attrs['GaugeLength']
+        headers = dict(h5_file['Acquisition'].attrs)
         return {'dt': dt, 
                 'nt': nt,
-                'nch': nch}
-    
+                'dx': dx,
+                'nch': nch,
+                'GL': GL,
+                'headers': headers}   
     else:
         ch1 = kwargs.pop('ch1', 0)
         ch2 = kwargs.pop('ch2', nch)
-        data = h5_file['Acquisition/Raw[0]/RawData/'][:,ch1:ch2].T
+        array_shape = h5_file['Acquisition/Raw[0]/RawData/'].shape
+        if array_shape[0] == nch:
+            data = h5_file['Acquisition/Raw[0]/RawData/'][ch1:ch2,:]
+        else:
+            data = h5_file['Acquisition/Raw[0]/RawData/'][:, ch1:ch2].T
         return data
-
+    
+    
     
 def _read_das_tdms(fname, **kwargs):
     
@@ -51,11 +62,13 @@ def _read_das_tdms(fname, **kwargs):
         dt = 1./tdms_file.properties['SamplingFrequency[Hz]']
         dx = tdms_file.properties['SpatialResolution[m]']
         nt = len(tdms_file['Measurement']['0'])
+        GL = tdms_file.properties['GaugeLength']
         headers = tdms_file.properties
         return {'dt': dt, 
                 'nt': nt,
                 'dx': dx, 
                 'nch': nch,
+                'GL': GL,
                 'headers': headers}
     else:
         ch1 = kwargs.pop('ch1', 0)
@@ -91,6 +104,12 @@ def das_preprocess(data_in):
     data_out = signal.detrend(data_in)
     data_out = data_out - np.median(data_out, axis=0) 
     return data_out
+
+def tapering(data, alpha):
+    nt = data.shape[1]
+    window = signal.windows.tukey(nt, alpha)
+    data = data * window[None, :]
+    return data
 
 
 def bandpass(data, dt, fl, fh):
